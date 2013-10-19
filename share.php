09b626239e646a419637594a76b2b88f1652152c
Harshad Sabne <harshadsabne@users.noreply.github.com>
2013-10-19 23:33:12 +0530

    Update share.php
    Fix typo

--- share.php
+++ share.php
@@ -11,7 +11,7 @@ Template Name: Share
 	  <div class="row first">
 		<div class="span-full">
 		  <h2>Share Your Code</h2>
-		  <p>Thanks for your interest in sharing your code! If you are a developer employed by NASA or a NASA contractor and have open source code you wish to host on the NASA GitHub repository, email us at <a href="mailto:opensource@lists.nasa.gov">opensource@lists.nasa.gov</a>. If you aren't affiliated with NASA, we encourage your to clone, fork, modify, break, and improve the code that we've released thus far. We're still working out the proper methods to take user contributions to our code and will provide information on how to do so as it develops.</p>
+		  <p>Thanks for your interest in sharing your code! If you are a developer employed by NASA or a NASA contractor and have open source code you wish to host on the NASA GitHub repository, email us at <a href="mailto:opensource@lists.nasa.gov">opensource@lists.nasa.gov</a>. If you aren't affiliated with NASA, we encourage you to clone, fork, modify, break, and improve the code that we've released thus far. We're still working out the proper methods to take user contributions to our code and will provide information on how to do so as it develops.</p>
 		</div>
 	  </div>
 	<?php get_footer(); ?>
